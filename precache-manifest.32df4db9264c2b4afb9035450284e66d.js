4d8890eba86a30fc4d03289b3dbbde2777eaad86
Jordi Sicart <jordi.sicart@realimpactanalytics.com>
2019-05-18 14:04:22 +0200

    Updates

---
+++ precache-manifest.32df4db9264c2b4afb9035450284e66d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7b18152271cad15fb4d5e8e75b0bd21",
+    "url": "/11questions/index.html"
+  },
+  {
+    "revision": "822ab400c0c997a939b2",
+    "url": "/11questions/static/css/main.1d544b42.chunk.css"
+  },
+  {
+    "revision": "b564e4bbfe633f797c9b",
+    "url": "/11questions/static/js/2.ffa115d8.chunk.js"
+  },
+  {
+    "revision": "822ab400c0c997a939b2",
+    "url": "/11questions/static/js/main.d3b85083.chunk.js"
+  },
+  {
+    "revision": "5d54679db48e84a0b94a",
+    "url": "/11questions/static/js/runtime~main.c8dffc88.js"
+  }
+]);
