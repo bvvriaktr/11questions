a84b790e6d7386a45da5c686968e9947f73a7e1c
Jordi Sicart <jordi.sicart@realimpactanalytics.com>
2019-05-18 14:14:08 +0200

    add https

--- src/importData.js
+++ src/importData.js
@@ -1,5 +1,5 @@
 import {get} from 'lodash/fp'
-const url='http://spreadsheet-parser.herokuapp.com/?id=1aQ48d8wVCJtSoSCPTfYjN0h20Cf3yBxaSu8LRwenSmU';
+const url='https://spreadsheet-parser.herokuapp.com/?id=1aQ48d8wVCJtSoSCPTfYjN0h20Cf3yBxaSu8LRwenSmU';
 
 export const fetchData = () => fetch(url)
 .then(rawData => rawData.json())
